1a3be761505ea351da743ac432db0a6479a143f4
Shruti-Mishra27 <shrutimishra.2743@gmail.com>
2023-03-17 10:30:03 +0530

    Server updated

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+
+app.get("/",function(req,res){
+    res.send("Testing done");
+});
+
+
+app.listen(3000,function(){
+    console.log("Local port 3000 working");
+});
