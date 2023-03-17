85015d3b772cadae318adbf5b1a101e998be6a6c
Shruti-Mishra27 <shrutimishra.2743@gmail.com>
2023-03-17 13:00:31 +0530

    Extra folder deleted

--- Server/index.js
+++ Server/index.js
@@ -3,9 +3,19 @@ const app = express();
 
 
 app.get("/",function(req,res){
-    res.send("Testing done");
+    res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req,res){
+
+    var v2 = req.body.num2;
+    var v3 = req.body.num1;
+    var v1 = req.body.num1;
+    var v1 = req.body.num1;
+    // var result = n1 + n2;
+
+    res.send("Result of calculation(sum) is : " + result);
+});
 
 app.listen(3000,function(){
     console.log("Local port 3000 working");
