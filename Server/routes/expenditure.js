3c568b86f2ed1569460d411264715d55055be8c8
Murli Patel <patelmurli15@gmail.com>
2023-04-09 16:28:54 +0530

    revised routes

--- Server/routes/expenditure.js
+++ Server/routes/expenditure.js
@@ -17,60 +17,47 @@ router.get("/", authorize, async (req, res) => {
     }
   });
   
-//   //create a todo, using authorize middleware
-//   router.post("/todos", authorize, async (req, res) => {
-//     try {
-//       console.log(req.body);
-//       const { description } = req.body;
-//       const newTodo = await pool.query(
-//         "INSERT INTO todos (user_id, description) VALUES ($1, $2) RETURNING *",
-//         [req.user.id, description]
-//       );
+  //create a todo, using authorize middleware
+  router.post("/todos", authorize, async (req, res) => {
+    try {
+      console.log(req.body);
+      const { Loan_id ,
+        Ifsc_code ,
+        Loan_amount ,
+        Card_balance ,
+        Credit_score ,
+        Stock_name ,
+        Stock_price ,
+        Total_Stock_price} = req.body;
+      const newTodo = await pool.query(
+        "INSERT INTO expenditure (user_id,  Loan_id, Ifsc_code, Loan_amount, Card_balance, Credit_score, Stock_name, Stock_price, Total_Stock_price) VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9) RETURNING *",
+        [req.user.id, Loan_id, Ifsc_code, Loan_amount, Card_balance, Credit_score, Stock_name, Stock_price, Total_Stock_price]
+      );
   
-//       res.json(newTodo.rows[0]);
-//     } catch (err) {
-//       console.error(err.message);
-//     }
-//   });
+      res.json(newTodo.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
   
-//   //update a todo
-//   router.put("/todos/:id", authorize, async (req, res) => {
-//     try {
-//       const { id } = req.params;
-//       const { description } = req.body;
-//       const updateTodo = await pool.query(
-//         "UPDATE todos SET description = $1 WHERE todo_id = $2 AND user_id = $3 RETURNING *",
-//         [description, id, req.user.id]
-//       );
+  //delete a todo
+  router.delete("/todos/:id", authorize, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTodo = await pool.query(
+        "DELETE FROM expenditure WHERE exp = $1 AND user_id = $2 RETURNING *",
+        [id, req.user.id]
+      );
   
-//       if (updateTodo.rows.length === 0) {
-//         return res.json("This todo is not yours");
-//       }
+      if (deleteTodo.rows.length === 0) {
+        return res.json("This todo is not yours");
+      }
   
-//       res.json("Todo was updated");
-//     } catch (err) {
-//       console.error(err.message);
-//     }
-//   });
-  
-//   //delete a todo
-//   router.delete("/todos/:id", authorize, async (req, res) => {
-//     try {
-//       const { id } = req.params;
-//       const deleteTodo = await pool.query(
-//         "DELETE FROM todos WHERE todo_id = $1 AND user_id = $2 RETURNING *",
-//         [id, req.user.id]
-//       );
-  
-//       if (deleteTodo.rows.length === 0) {
-//         return res.json("This todo is not yours");
-//       }
-  
-//       res.json("Todo was deleted");
-//     } catch (err) {
-//       console.error(err.message);
-//     }
-//   });
+      res.json("Todo was deleted");
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
   
   
 module.exports = router;
--- Server/routes/finance_planning.js
+++ Server/routes/finance_planning.js
@@ -16,5 +16,40 @@ router.get("/", authorize, async (req, res) => {
       res.status(500).send("Server error");
     }
   });
-  
+
+  //create a todo, using authorize middleware
+router.post("/todos", authorize, async (req, res) => {
+  try {
+    console.log(req.body);
+    const { Bike_amount, Car_amount, Home_amount, Loan_with_interest } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todos (user_id, Bike_amount, Car_amount, Home_amount, Loan_with_interest) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [req.user.id, Bike_amount, Car_amount, Home_amount, Loan_with_interest]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo
+router.delete("/todos/:id", authorize, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query(
+      "DELETE FROM finance_planning WHERE f_id = $1 AND user_id = $2 RETURNING *",
+      [id, req.user.id]
+    );
+
+    if (deleteTodo.rows.length === 0) {
+      return res.json("This todo is not yours");
+    }
+
+    res.json("Todo was deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 module.exports = router;
