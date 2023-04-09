6c04ea5357c3f44bb8c92cf6ab373ab9ae82669f
Murli Patel <patelmurli15@gmail.com>
2023-04-09 11:27:11 +0530

    index.js updated

--- Server/index.js
+++ Server/index.js
@@ -1,30 +1,18 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-// const request = require("request");
-const login = '../Client/login and signup/'
 const app = express();
+const cors = require("cors");
 
-// app.use(express.static("Client"));
-app.use(express.static("D:/0000 gfg hackethon/gfg_cloudhackethon/Client/login and signup/"));
-app.use(bodyParser.urlencoded({extended:true}));
+//middleware
+app.use(cors());
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    // res.sendFile(__dirname + "/login.html");
-    res.sendFile("D:/0000 gfg hackethon/gfg_cloudhackethon/Client/login and signup/login.html");
-    // res.sendFile( login + "/login.html");
-    // res.sendFile(login)
-});
+//routes
+app.use("/authentication", require("./routes/jwtAuth"));
+app.use("/dashboard", require("./routes/dashboard"));
 
-app.post("/", function(req,res){
+app.use('/expenditure',require("./routes/expenditure"))
+app.use('/finance',require("./routes/finance_planning"))
 
-    var name = req.body.fullName;
-    var phone = Number(req.body.phNumber);
-    var email = req.body.eMail;
-    var pw = req.body.passWord;
-
-    console.log(name,phone,email,pw);
-});
-
-app.listen(3000,function(){
-    console.log("Local port 3000 working");
+app.listen(5000, () => {
+  console.log(`Server is starting on port 5000`);
 });
