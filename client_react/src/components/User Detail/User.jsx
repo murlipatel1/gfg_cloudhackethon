030b8efc5cf5a1750a7a2d53e8cacb3d13d316df
Shruti Mishra <96042938+Shruti-Mishra27@users.noreply.github.com>
2023-05-17 16:12:21 +0530

    Add Login-Signup & User

---
+++ client_react/src/components/User Detail/User.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import "./user.css";
+import SideBar from "../SideBar/SideBar";
+const User = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <SideBar active={"users"} />
+      <div>
+        <div className="user-container">
+          <h1>User Profile</h1>
+          <div className="sub-container1">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" />
+
+            <label for="phone">Phone:</label>
+            <input type="tel" id="phone" name="phone" maxlength="10" />
+          </div>
+          <div className="sub-container1">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" />
+
+            <label for="address">Address:</label>
+            <textarea id="address" name="address"></textarea>
+          </div>
+          <div className="sub-container1">
+            <label for="aadhar">Aadhar VID:</label>
+            <input type="text" id="aadhar" name="aadhar" maxlength="16" />
+
+            <label for="pan">PAN:</label>
+            <input type="text" id="pan" name="pan" maxlength="10" />
+          </div>
+
+          <div onClick={handleOpenModal} className="notification">
+            🔔
+          </div>
+          {showModal && (
+            <div className="modal">
+              <div className="modal-content">
+                <span className="close" onClick={handleCloseModal}>
+                  {/* &times; */}
+                </span>
+                <div class="notifications-container">
+                  <span className="notification-message">
+                    Your notification message goes here
+                  </span>
+                </div>
+              </div>
+            </div>
+          )}
+          {/* <div className="container preferences-container"> */}
+          <hr className="myhr" />
+          <h1>Preferences </h1>
+          <h3 className="notify">Notifications:</h3>
+          <div className="sub-container2">
+            <div className="preference-options">
+              <label for="email-notifications">Email</label>
+              <input
+                type="checkbox"
+                id="email-notifications"
+                name="email-notifications"
+              />
+              <label for="sms-notifications"> SMS</label>
+              <input
+                type="checkbox"
+                id="sms-notifications"
+                name="sms-notifications"
+              />
+            </div>
+          </div>
+          {/* <hr className="myhr1" /> */}
+          <h3 className="notify">Alerts:</h3>
+          <div className="sub-container2">
+            <div className="preference-options">
+              <label for="email-alerts">Email</label>
+              <input type="checkbox" id="email-alerts" name="email-alerts" />
+              <label for="sms-alerts">SMS</label>
+              <input type="checkbox" id="sms-alerts" name="sms-alerts" />
+            </div>
+          </div>
+          <hr className="myhr" />
+          <h2 className="other">Other Settings</h2>
+          <div className="sub-container2">
+            <div className="preference-options">
+              <label for="language">Language:</label>
+              <select id="language" name="language">
+                <option value="english">English</option>
+                <option value="spanish">Hindi</option>
+                <option value="french">Gujarati</option>
+              </select>
+            </div>
+            <div className="preference-options">
+              <label for="theme">Theme:</label>
+              <select id="theme" name="theme">
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="sub-container2">
+            <button className="change-password"> Save </button>
+          </div>
+          {/* </div> */}
+          <hr className="myhr" />
+          <div className="sub-container1">
+            <button className="change-password">Change Password</button>
+            <button className="delete-account">Delete Account</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default User;
---
+++ client_react/src/components/login and signup/Login.jsx
@@ -0,0 +1,201 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+import image from "../login and signup/gradient.png";
+import { Icon, InlineIcon } from "@iconify/react";
+const Login = () => {
+  let navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    navigate("/expendeture");
+  };
+
+  const [input1Value, setInput1Value] = useState("");
+  const [input2Value, setInput2Value] = useState("");
+  let colour = "black";
+  let icon = "mdi:password";
+
+  function handleInputChange(event) {
+    if (event.target.name === "password") {
+      setInput1Value(event.target.value);
+    } else if (event.target.name === "confirmpassword") {
+      setInput2Value(event.target.value);
+    }
+  }
+  const isPasswordMatch = input1Value === input2Value;
+
+  return (
+    <>
+      {/* <div>Login</div> */}
+      <div className="body2l">
+        <div className="containerl">
+          <input type="checkbox" id="flip" />
+          <div className="cover">
+            <div className="front">
+              <img src={image} alt="" />
+              <div className="text">
+                <span className="text-1">JOIN US</span>
+                <span className="text-2">Start a Valuable Journey With Us</span>
+              </div>
+            </div>
+            <div className="back">
+              <img className="backImg" src={image} alt="" />
+              <div className="text">
+                <br />
+                <span className="text-1">
+                  Complete miles of journey <br /> with one step
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="forms">
+            <div className="form-content">
+              <div className="login-form">
+                <div className="title">Login</div>
+                <form action="#">
+                  <div className="input-boxes">
+                    <div className="input-box">
+                      <Icon
+                        icon={"mdi:email"}
+                        color="black"
+                        className="text-3xl"
+                      />
+                      {/* //text to ntext */}
+                      <input type="etext" placeholder="Email-Id" required />
+                    </div>
+                    <div className="input-box">
+                      <Icon
+                        icon={"material-symbols:lock"}
+                        color="black"
+                        className="text-3xl"
+                      />
+                      <input type="password" placeholder="Password" required />
+                    </div>
+
+                    <div className="text">
+                      <a href="#">Forgot password?</a>
+                    </div>
+
+                    <div className="button input-box">
+                      <input
+                        type="submit"
+                        value="Login"
+                        onClick={handleSubmit}
+                      />
+                    </div>
+
+                    <div className="text sign-up-text">
+                      {" "}
+                      Don't have an account?
+                      <label for="flip">Register Now</label>
+                    </div>
+                  </div>
+                </form>
+              </div>
+
+              <div action="/" className="register-form">
+                <div className="title">REGISTER</div>
+
+                <form action="#">
+                  <div className="input-boxes">
+                    <div className="input-box">
+                      <Icon
+                        icon={"material-symbols:person"}
+                        color="black"
+                        className="text-3xl"
+                      />
+                      <input
+                        type="ntext" //text to ntext
+                        name="fullName"
+                        placeholder="Name"
+                        required
+                      />
+                    </div>
+
+                    <div className="input-box">
+                      <Icon
+                        icon={"material-symbols:phone-callback-sharp"}
+                        color="black"
+                        className="text-3xl"
+                      />
+                      <input
+                        type="number"
+                        name="phNumber"
+                        placeholder="Phone"
+                        required
+                      />
+                    </div>
+
+                    <div className="input-box">
+                      <Icon
+                        icon={"mdi:email"}
+                        color="black"
+                        className="text-3xl"
+                      />
+                      <input
+                        type="etext" //text to etext
+                        name="email"
+                        placeholder="Email-Id"
+                        required
+                      />
+                    </div>
+
+                    <div className="input-box">
+                      <Icon
+                        icon={"mdi:password"}
+                        color="black"
+                        className="text-3xl"
+                      />
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={input1Value}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+
+                    <div className="input-box">
+                      {isPasswordMatch ? (
+                        <Icon
+                          icon={"material-symbols:check-circle-outline-rounded"}
+                          color={"green"}
+                          className="text-3xl"
+                        />
+                      ) : (
+                        <Icon
+                          icon={"mdi:password"}
+                          color={"black"}
+                          className="text-3xl"
+                        />
+                      )}
+                      <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="confirmpassword"
+                        value={input2Value}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+
+                    <div className="button input-box">
+                      <input type="submit" value="Register" />
+                    </div>
+
+                    <div className="text login-text">
+                      Already have an account?
+                      <label for="flip">Login now</label>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
