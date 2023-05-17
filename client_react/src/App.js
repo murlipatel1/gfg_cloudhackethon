b6a65e6097ecb576fed71e029b0fb702d139d2c1
Shruti Mishra <96042938+Shruti-Mishra27@users.noreply.github.com>
2023-05-17 16:07:10 +0530

    Add App.js

---
+++ client_react/src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Expendeture from "./components/Expendeture/Expendeture";
+import Calc from "./components/Finance Calculator/Calc";
+import Plan from "./components/Finance Planning/Plan";
+// import HomePage from "./components/HomePage";
+import Login from "./components/login and signup/Login";
+import User from "./components/User Detail/User";
+import Stats from "./components/Statistics/stats";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/expendeture" element={<Expendeture />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/calc" element={<Calc />} />
+        <Route exact path="/plan" element={<Plan />} />
+        <Route exact path="/stats" element={<Stats />} />
+        <Route exact path="/users" element={<User />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
