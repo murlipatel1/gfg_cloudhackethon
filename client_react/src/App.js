302289d582fafd45e41b726c0b5df197ba88fc31
Shruti Mishra <96042938+Shruti-Mishra27@users.noreply.github.com>
2023-05-17 23:45:08 +0530

    Update App.js

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -2,16 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Expendeture from "./components/Expendeture/Expendeture";
 import Calc from "./components/Finance Calculator/Calc";
 import Plan from "./components/Finance Planning/Plan";
-// import HomePage from "./components/HomePage";
 import Login from "./components/login and signup/Login";
 import User from "./components/User Detail/User";
 import Stats from "./components/Statistics/stats";
+import HomePage from "./components/Home/HomePage";
 
 function App() {
   return (
     <>
-      <Routes> 
-        <Route exact path="/" element={<Login />} />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
         <Route exact path="/expendeture" element={<Expendeture />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/calc" element={<Calc />} />
