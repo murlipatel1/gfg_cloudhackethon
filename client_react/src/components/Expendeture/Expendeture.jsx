948a87ceac6f9acba5f536d2ad27207c8f7f461e
Shruti-Mishra27 <shrutimishra.2743@gmail.com>
2023-05-18 16:20:33 +0530

    Add Counter in Expenditure

--- client_react/src/components/Expendeture/Expendeture.jsx
+++ client_react/src/components/Expendeture/Expendeture.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./expendeture.css";
 import SideBar from "../SideBar/SideBar";
 import Navbar from "../Navbar/navbar";
 
 const Expendeture = () => {
+  const [loanCounter, setLoanCounter] = useState(0);
+  const [cardCounter, setCardCounter] = useState(0);
+  const [stockCounter, setStockCounter] = useState(0);
+  const [mfCounter, setMFCounter] = useState(0);
+
+  const increaseLoan = () => {
+    setLoanCounter(loanCounter + 1);
+  };
+
+  const increaseCard = () => {
+    setCardCounter(cardCounter + 1);
+  };
+
+  const increaseStock = () => {
+    setStockCounter(stockCounter + 1);
+  };
+
+  const increaseMF = () => {
+    setMFCounter(mfCounter + 1);
+  };
   return (
     <>
       <Navbar active={"expendeture"} />
@@ -15,7 +35,9 @@ const Expendeture = () => {
               <h1>Expenditure</h1>
               <hr className="myhr" />
               <div>
-                <h2>Loan</h2>
+                <h2>
+                  Loan <span className="counter">{loanCounter}</span>
+                </h2>
                 <br />
                 <div className="sub-container">
                   <label for="name">Unique Id:</label>
@@ -27,63 +49,130 @@ const Expendeture = () => {
                 </div>
                 <div className="sub-container">
                   <label for="email">Amount:</label>
-                  <input type="text" id="loanamount" name="loanamount" />
+                  <input
+                    type="number"
+                    id="loanamount"
+                    name="loanamount"
+                    placeholder="Rs"
+                  />
                 </div>
-
+                <button className="change-password" onClick={increaseLoan}>
+                  Submit
+                </button>
                 {/* <!-- <div className="notification">!</div> --> */}
                 {/* </div> */}
                 <hr className="myhre" />
                 <div className="">
-                  <h2>Cards</h2>
+                  <h2>
+                    Cards <span className="counter">{cardCounter}</span>
+                  </h2>
                   <br />
                   <div className="sub-container">
-                    <label for="name">Balance:</label>
-                    <input type="text" id="name" name="name" />
+                    {/* <label for="name">Card Type:</label>
+                    <input type="text" id="name" name="name" /> */}
+                    <label htmlFor="cardType">Card Type:</label>
+                    <select
+                      id="cardType"
+                      name="cardType"
+                      className="dropbox-menu"
+                    >
+                      <option value="visa">Visa</option>
+                      <option value="mastercard">Mastercard</option>
+                      <option value="amex">American Express</option>
+                      <option value="discover">Discover</option>
+                    </select>
                   </div>
                   <div className="sub-container">
-                    <label for="name">Credit Score:</label>
-                    <input type="text" id="name" name="name" />
+                    <label for="phone"> Card Number:</label>
+                    <input
+                      type="text"
+                      id="number"
+                      name="number"
+                      maxLength={16}
+                    />
+                  </div>
+                  <div className="sub-container">
+                    <label for="name">Total Amount:</label>
+                    <input
+                      type="number"
+                      id="total-quantity"
+                      name="total-quantity"
+                      placeholder="Rs"
+                    />
                   </div>
                 </div>
               </div>
+              <button className="change-password" onClick={increaseCard}>
+                Submit
+              </button>
             </div>
             <div className="expendeturecontainer">
               <h1>Stock Management</h1>
 
               <hr className="myhr" />
               <div>
-                <h2>Stock</h2>
+                <h2>
+                  Stock <span className="counter">{stockCounter}</span>
+                </h2>
                 <br />
                 <div className="sub-container">
-                  <label for="name">Stocks:</label>
+                  <label for="name">Stock Name:</label>
                   <input type="text" id="name" name="name" />
                 </div>
                 <div className="sub-container">
                   <label for="phone"> Stock Price:</label>
-                  <input type="tel" id="phone" name="phone" />
+                  <input
+                    type="number"
+                    id="stock-price"
+                    name="stock-price"
+                    placeholder="Rs"
+                  />
                 </div>
                 <div className="sub-container">
-                  <label for="name">Total amount:</label>
-                  <input type="text" id="name" name="name" />
+                  <label for="name">Total Amount:</label>
+                  <input
+                    type="number"
+                    id="total-quantity"
+                    name="total-quantity"
+                    placeholder="Rs"
+                  />
                 </div>
               </div>
+              <button className="change-password" onClick={increaseStock}>
+                Submit
+              </button>
               <hr className="myhre" />
               <div>
-                <h2>Mutual Funds</h2>
+                <h2>
+                  Mutual Funds <span className="counter">{mfCounter}</span>
+                </h2>
                 <br />
                 <div className="sub-container">
-                  <label for="name">Stocks:</label>
+                  <label for="name">Stock Name:</label>
                   <input type="text" id="name" name="name" />
                 </div>
                 <div className="sub-container">
                   <label for="phone"> Stock Price:</label>
-                  <input type="tel" id="phone" name="phone" />
+                  <input
+                    type="number"
+                    id="stock-price"
+                    name="stock-price"
+                    placeholder="Rs"
+                  />
                 </div>
                 <div className="sub-container">
-                  <label for="name">Total amount:</label>
-                  <input type="text" id="name" name="name" />
+                  <label for="name">Total Amount:</label>
+                  <input
+                    type="number"
+                    id="total-quantity"
+                    name="total-quantity"
+                    placeholder="Rs"
+                  />
                 </div>
               </div>
+              <button className="change-password" onClick={increaseMF}>
+                Submit
+              </button>
             </div>
           </div>
           <div className="sub-container buttonalignment">
